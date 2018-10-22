dcaf0807d24e90e738482da69c0d1bfcc1549310
c0493825 <antproenca92@gmail.com>
2018-10-21 23:34:52 -0300

    Commit de todas as funcionalidades. App comunicando-se com os end points, inserindo, deletando, filtrando tarefas.

---
+++ src/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageReader from '../template/pageHEader'
+
+
+export default props => (
+    <div>
+        <PageReader name='Sobre' small='Nós'></PageReader>
+        <h2>Nossa história</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Missão e visão</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+)
